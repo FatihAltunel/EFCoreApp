84e208c92cde84236141076b3a4af9f883274947
Fatih Altunel <jjrousseau1128@gmail.com>
2024-05-01 16:05:04 +0300

    Database connection formed

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,8 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EFCoreApp.Data
 {
     public class DataContext:DbContext{
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=database.db");
+        }
+
         public DbSet<Student>Students => Set<Student>();
         public DbSet<Course>Courses => Set<Course>();
         public DbSet<CourseRegister>CourseRegisters => Set<CourseRegister>();
--- obj/Debug/net8.0/EFCoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EFCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c6b5f321be2e739423b5873da864c73388134b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ab31ba492be1f415c8ff6ecbbe2bd9dfaba1e63")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
